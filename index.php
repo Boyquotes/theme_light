4c5cf2288da7597ea692c50e2d114392c3572827
Boyquotes <webmaster@linux-live-cd.org>
2016-03-02 10:18:13 +0100

    2 wp_query -> News et Candidat, à vous pour formation et la récupération des 4 pages

--- index.php
+++ index.php
@@ -1,82 +1,83 @@
 <?php get_header(); ?>
 <div id="main">
     <div id="content">
-        <h1>Contenu Principal</h1>
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<h1><?php the_title(); ?></h1>
-		<h4>Posted on <?php the_time('F jS, Y') ?></h4>
-		<p><?php the_content(__('(more...)')); ?></p>
-		<hr> <?php endwhile; else: ?>
-		<p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
-    </div>
-	<?php get_sidebar(); ?>
-</div>
 
-<div id="delimiter">
-</div>
-
-<div class="news">
-<?php
-	$args = array(
-		'post_type'  => 'post',
-		'category_name' => 'news',
-	);
-
+    <div class="news">
+      <?php
+      	// 1 Parametrage de la requete dans un tableau $args
+      	$args = array(
+      		'post_type'  => 'post',
+      		'category_name' => 'news',
+      		'posts_per_page' => '2',
+      	);
 
         // 2. on exécute la query
         $my_query = new WP_Query($args);
 
-        #LA BOUCLE
+        # 3.LA BOUCLE
         // 3. on lance la boucle WHILE!
         if($my_query->have_posts()) : while ($my_query->have_posts() ) : $my_query->the_post();
-		the_title();
-		the_content();
+      		the_title();
+      		the_content();
         endwhile;
         endif;
 
-?>
+        ?>
+        <!-- fin news -->
+      </div>
 
-</div>
+      <h2> Les candidats </h2>
+      <div class="candidat">
+      	<?php
+        // 1 Parametrage de la requete dans un tableau $args
+        $args = array(
+          'post_type'  => 'candidat',
+          'posts_per_page' => '3',
+        );
+      	// 2. on exécute la query
+      	$my_query = new WP_Query($args);
+
+      	#LA BOUCLE
+      	// 3. on lance la boucle WHILE!
+      	if($my_query->have_posts()) : while ($my_query->have_posts() ) : $my_query->the_post();
+      	?>
+          <p>
+          	<?php
+          	    the_title();
+          	    echo "</br>DEBUG ID du post :".get_the_ID()." DEBUG</br>";
+              	    $prenom = get_post_meta(get_the_ID(), '_ma_valeur_prenom', true);
+              	    if(isset($prenom)){
+          		          echo " Prénom";
+                  	    echo $prenom;
+                        echo "</br>";
+              	    }
+
+          	    $heros = get_post_meta(get_the_ID(), '_ma_valeur_heroes', true);
+          	    if(isset($heros) && $heros != ""){
+          		      echo " Son héros : ";
+                  	echo $heros;
+                    echo "</br>";
+              	}
+              	else{
+          		      echo "Il n'a pas rempli le champs héros";
+              	}
+          	?>
+          </p>
+          <?php
+          endwhile;
+          endif;
+          ?>
+          <!-- fin candidat -->
+        </div>
 
 
-<h2> Les candidats </h2>
-<div class="candidat">
-	<?php
+<!-- fin content -->
+    </div>
 
-	// 2. on exécute la query
-	$my_query = new WP_Query($args);
-	
-	#LA BOUCLE
-	// 3. on lance la boucle WHILE!
-	if($my_query->have_posts()) : while ($my_query->have_posts() ) : $my_query->the_post();
-	?>
-
-	<?php    
-	    the_title();
-	    echo "ID du post :".get_the_ID()."</br>";
-    	    $prenom = get_post_meta(get_the_ID(), '_ma_valeur_prenom', true);
-    	    if(isset($prenom)){
-		echo " ";
-        	echo $prenom;
-    	    }
-
-	    $heros = get_post_meta(get_the_ID(), '_ma_valeur_heroes', true);
-	    var_dump($heros); 
-	    if(isset($heros) && $heros != ""){
-		echo " ";
-        	echo $heros;
-    	    }
-    	    else{
-		echo "No heros";
-    	    }
-
-	?>
-	</div>
-	<?php
-	endwhile;
-	endif;
-	?>
-
+      <?php get_sidebar(); ?>
+      <div id="delimiter">
+      </div>
+<!-- fin main -->
 </div>
 
 
